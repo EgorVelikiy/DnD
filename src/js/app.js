82425d82460e72d0674759b412579f1654645ea5
Egor <egor.velikiy.02@mail.ru>
2024-11-10 20:39:13 +0300

    Result

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,30 @@
-import List from './components/list_create/list_create'
-import DnD from './components/DnD/DnD'
+import List from './components/list_create/list_create';
+import Sortable from 'sortablejs';
 
-const lists = document.querySelectorAll('.list-container')
+const lists = document.querySelectorAll('.list-container');
 
 lists.forEach((li) => {
-  const newList = new List(li)
-  newList.bindDOM()
-})
+  const newList = new List(li);
+  newList.bindDOM();
 
-const listBody = document.querySelector('.container')
-const newDnD = new DnD(listBody)
-newDnD.onMouseDown();
-newDnD.onMouseMove();
-newDnD.onMouseUp();
-newDnD.getCardBack();
+  const body = li.querySelector('.list-body');
+  new Sortable(body, {
+    group: 'shared',
+    ghostClass: 'rotating',
+    animation: 150,
+    onStart: function (e) {
+      let itemEl = e.item;
+      this.oldlist = itemEl.closest('.list-container');
+      this.oldlistBody = this.oldlist.querySelector('.list-body');
+      console.log(this.oldlist.dataset.locStorKey);
+    },
+    onEnd: function (e) {
+      let itemEl = e.item;
+      let newList = itemEl.closest('.list-container');
+      let newListBody = newList.querySelector('.list-body');
+
+      localStorage.setItem(this.oldlist.dataset.locStorKey, this.oldlistBody.innerText);
+      localStorage.setItem(newList.dataset.locStorKey, newListBody.innerText);
+    },
+  });
+});
--- src/js/components/list_create/list_create.js
+++ src/js/components/list_create/list_create.js
@@ -1,24 +1,24 @@
 export default class List {
-    constructor(list) {
-        this._list = list;
-    }
+  constructor(list) {
+    this._list = list;
+  }
 
-    static listMarkup(listName, locStorKey) {
-        return `
+  static listMarkup(listName, locStorKey) {
+    return `
             <div class="list">
                 <div class="header">
                     <h1 class="header-title">${listName}</h1>
                 </div>
                 <div class="list-body data-key=${locStorKey}">
-                    <div class="card-create-active hide">
-                        <div class="card-create-active card-opened">
-                            <textarea class="card-create-textarea hover" placeholder="Ввести заголовок для этой карточки"></textarea>
-                        </div>
-                        <div class="card-create-tools">
-                            <div class="tools">
-                                <input class="add-new-card hover" type="button" value="Добавить карточку">
-                                <button class="close-card hover"></button>
-                            </div>
+                </div>
+                <div class="card-create-active hide">
+                    <div class="card-create-active card-opened">
+                        <textarea class="card-create-textarea hover" placeholder="Ввести заголовок для этой карточки"></textarea>
+                    </div>
+                    <div class="card-create-tools">
+                        <div class="tools">
+                            <input class="add-new-card hover" type="button" value="Добавить карточку">
+                            <button class="close-card hover"></button>
                         </div>
                     </div>
                 </div>
@@ -29,12 +29,12 @@ export default class List {
                     </a>
                 </div>
             </div>
-        `
-    }
+        `;
+  }
 
-    static cardMarkup(title) {
-        if (title) {
-            return `
+  static cardMarkup(title) {
+    if (title) {
+      return `
                 <a class="list-card hover" href="#">
                     <div class="list-card-details">
                         <span class="list-card-title">
@@ -43,95 +43,92 @@ export default class List {
                     </div>
                     <div class="list-card-remover hover"></div>
                 </a>
-            `
-        }
-
-        return
+            `;
     }
 
-    bindDOM() {
-        const { listName, locStorKey } = this._list.dataset;
-        this._list.innerHTML = this.constructor.listMarkup(listName, locStorKey);
+    return;
+  }
 
-        const key = localStorage.getItem(locStorKey);
-        const cardCreateActive = this._list.querySelector('.card-create-active');
+  bindDOM() {
+    const { listName, locStorKey } = this._list.dataset;
+    this._list.innerHTML = this.constructor.listMarkup(listName, locStorKey);
+    const listBody = this._list.querySelector('.list-body');
 
-        if(key) {
-            this.init(cardCreateActive, key)
-        }
+    const key = localStorage.getItem(locStorKey);
+    const cardCreateActive = this._list.querySelector('.card-create-active');
 
-        const cardCreateContainer = this._list.querySelector('.card-create-container');
-        const CardCreateTextArea = this._list.querySelector('.card-create-textarea');
-
-
-        cardCreateContainer.addEventListener('click', () => {
-            this.onAddCardClick(cardCreateContainer, cardCreateActive, CardCreateTextArea)
-        })
-
-        const closeAddCard = this._list.querySelector('.close-card');
-
-        closeAddCard.addEventListener('click', () => {
-            this.onCloseCardClick(cardCreateContainer, cardCreateActive)
-        })
-
-        const addNewCardBtn = this._list.querySelector('.add-new-card');
-        const listBody = this._list.querySelector('.list-body')
-        
-        addNewCardBtn.addEventListener('click', () => {
-            this.addNewCard(listBody, CardCreateTextArea, cardCreateActive, locStorKey)
-        })
-
-
-        listBody.addEventListener('click', (e) => {
-            this.cardDelete(e, listBody, locStorKey)
-        })
-
+    if (key) {
+      this.init(listBody, key);
     }
 
-    init(cardCreateActive, key) {
-        let markup = ""
-        const titles = key.split('\n')
+    const cardCreateContainer = this._list.querySelector('.card-create-container');
+    const CardCreateTextArea = this._list.querySelector('.card-create-textarea');
 
-        titles.forEach(title => {
-            markup += this.constructor.cardMarkup(title)
-        });
+    cardCreateContainer.addEventListener('click', () => {
+      this.onAddCardClick(cardCreateContainer, cardCreateActive, CardCreateTextArea);
+    });
 
-        cardCreateActive.insertAdjacentHTML("beforebegin", markup);
+    const closeAddCard = this._list.querySelector('.close-card');
+
+    closeAddCard.addEventListener('click', () => {
+      this.onCloseCardClick(cardCreateContainer, cardCreateActive);
+    });
+
+    const addNewCardBtn = this._list.querySelector('.add-new-card');
+
+    addNewCardBtn.addEventListener('click', () => {
+      this.addNewCard(listBody, CardCreateTextArea, locStorKey);
+    });
+
+    listBody.addEventListener('click', (e) => {
+      this.cardDelete(e, listBody, locStorKey);
+    });
+  }
+
+  init(listBody, key) {
+    let markup = '';
+    const titles = key.split('\n');
+
+    titles.forEach((title) => {
+      markup += this.constructor.cardMarkup(title);
+    });
+
+    listBody.insertAdjacentHTML('beforeend', markup);
+  }
+
+  onAddCardClick(container, active, textarea) {
+    container.classList.add('hide');
+    active.classList.remove('hide');
+    textarea.focus();
+  }
+
+  onCloseCardClick(container, active) {
+    container.classList.remove('hide');
+    active.classList.add('hide');
+  }
+
+  addNewCard(listBody, textarea, locStorKey) {
+    const cardTitle = textarea.value;
+    if (cardTitle) {
+      const markup = this.constructor.cardMarkup(cardTitle);
+      listBody.insertAdjacentHTML('beforeend', markup);
+      localStorage.setItem(locStorKey, listBody.innerText);
+      textarea.value = '';
+    } else {
+      textarea.focus();
     }
 
-    onAddCardClick(container, active, textarea) {
-        container.classList.add('hide');
-        active.classList.remove('hide')
-        textarea.focus()
+    this.closeAddCard = this._list.querySelector('.close-card');
+  }
+
+  cardDelete(event, listBody, locStorKey) {
+    const { target } = event;
+    if (target.className.includes('.list-card-remover'.slice(1))) {
+      target.closest('.list-card').remove();
     }
 
-    onCloseCardClick(container, active) {
-        container.classList.remove('hide');
-        active.classList.add('hide')
+    if (listBody.children.length > 0) {
+      localStorage.setItem(locStorKey, listBody.innerText);
     }
-
-    addNewCard(listBody, textarea, cardCreateActive, locStorKey) {
-        const cardTitle = textarea.value;
-        if (cardTitle) {
-            const markup = this.constructor.cardMarkup(cardTitle);
-            cardCreateActive.insertAdjacentHTML("beforebegin", markup);
-            localStorage.setItem(locStorKey, listBody.innerText);
-            textarea.value = "";
-        } else {
-            textarea.focus();
-        }
-
-        this.closeAddCard = this._list.querySelector('.close-card');
-    }
-
-    cardDelete(event, listBody, locStorKey) {
-        const { target } = event;
-        if (target.className.includes('.list-card-remover'.slice(1))) {
-            target.closest('.list-card').remove();
-        }
-
-        if (listBody.children.length > 0) {
-            localStorage.setItem(locStorKey, listBody.innerText);
-        }
-    }
+  }
 }
